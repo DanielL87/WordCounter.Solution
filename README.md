ebea50595f06aabd4e65797ff05e66ce04416063
Daniel <Daniel>
2018-11-16 16:33:19 -0800

    adds 3 functions and tests for each

---
+++ README.md
@@ -0,0 +1,30 @@
+Word Counter
+
+Independent Project for Epicodus
+
+by: Daniel Lira , 11/16/18
+
+Description:
+Program has user enter Word of Choice, then Enters a Sentence, program should then split up the sentence and compare each word to initial word. Finally, program should output number of instances of first words found in sentence.
+
+
+Set-up
+Clone This Repository or Click on the Link Below.
+Then Simply enter the inofrmation asked for and see Awesome-O
+Respond in Kind
+
+https://daniell87.github.io/beep-boop/.
+
+Technologies Used:
+- Git
+- Atom
+- C#
+
+
+Known Bugs
+** None known at this Time **
+
+License
+Free for Public Use
+
+Please feel free to contact me at devidra87@gmail.com
--- WordCount/Models/Counts.cs
+++ WordCount/Models/Counts.cs
@@ -1,5 +1,28 @@
-namespace counter{
-  program class wordcounter{
-    
+namespace Wordcounter
+{
+  public class WordCount
+  {
+    public string wordInput(string word)
+    {
+      string userWord = word;
+      return userWord;
+    }
+    public string sentenceInput(string sentence)
+    {
+      string [] userSentenceSplit = sentence.Split(' ');
+      return userSentenceSplit;
+    }
+    public int countWords(string userWord, string userSentenceSplit)
+    {
+      int counter = 0;
+      for (int x = 0; x <= userSentenceSplit.Length; x++)
+      {
+        if(userSentenceSplit[x] == userWord)
+        {
+          counter++;
+        }
+          return counter;
+      }
+    }
   }
-}
+}  
