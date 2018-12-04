14f3ee5cfcc1e0dab50672fae8f7eb6967f86644
Maxwell Dubin <maxdhs@gmail.com>
2018-12-03 18:35:30 -0800

    add 2 more tests

--- WordCounter.Tests/ModelTests/CountsTests.cs
+++ WordCounter.Tests/ModelTests/CountsTests.cs
@@ -6,9 +6,8 @@ using System;
 namespace WordApp.Tests
 {
   [TestClass]
-  public class WordCounterTest
+  public class WordCounterTest 
   {
-
     [TestMethod]
     public void InputConstructor_TypeOfInput_Type()
     {
@@ -16,15 +15,31 @@ namespace WordApp.Tests
       Assert.AreEqual(typeof(WordCounter), newWord.GetType());
     } 
 
-   [TestMethod]
+    [TestMethod]
+    public void WordConstructor_CreatesInstanceOfWord_String()
+    {
+      string str = "beetles";
+      WordCounter newWord = new WordCounter(str, "test");
+      Assert.AreEqual("beetles", newWord.GetWord());
+    } 
+
+    [TestMethod]
+    public void SentenceConstructor_CreatesInstanceOfSentence_String()
+    {
+      string str = "beetles are bugs";
+      WordCounter newSentence = new WordCounter("test", str);
+      Assert.AreEqual("beetles are bugs", newSentence.GetSentence());
+    } 
+
+    [TestMethod]
     public void Counter_CheckForInstanceOfWordInSentence_int()
     {
-
       string word = "dog";
-      string sentence = "My dog is a dog";
+      string sentence = "My dog is a dog with a dog who is also a dog";
       WordCounter myWord = new WordCounter(word, sentence);
 
-      Assert.AreEqual(2, myWord.CountWords(word,sentence));
+      Assert.AreEqual(4, myWord.CountWords());
+      
     }  
   }
 }
